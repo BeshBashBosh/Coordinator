c5adf3a109f3d6bb6592c4867917ac5c968c28b6
Benjamin Hall <b.e.s.hall@outlook.com>
2019-01-13 18:23:51 +0000

    Set up initial view for tableview build from basic Friend model datasource

---
+++ FriendZone/Friend.swift
@@ -0,0 +1,14 @@
+//
+//  Friend.swift
+//  FriendZone
+//
+//  Created by beshbashbosh on 13/01/2019.
+//  Copyright © 2019 beshbashbosh. All rights reserved.
+//
+
+import Foundation
+
+struct Friend: Codable {
+    var name = "New Friend"
+    var timeZone = TimeZone.current
+}
--- FriendZone/ViewController.swift
+++ FriendZone/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var friends = [Friend]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    override func tableView(_ tableView: UITableView,
+                         numberOfRowsInSection section: Int) -> Int {
+        return friends.count
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                         cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let friend = friends[indexPath.row]
+        cell.textLabel?.text = friend.name
+        cell.detailTextLabel?.text = friend.timeZone.identifier
+        
+        return cell
+    }
 }
 
