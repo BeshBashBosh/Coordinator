54151f72c5a9d0758f47952bc39b441f58010e52
Benjamin Hall <b.e.s.hall@outlook.com>
2019-01-13 19:01:51 +0000

    Add tableview delegate and datasource methods for FriendViewController

--- FriendZone/FriendViewController.swift
+++ FriendZone/FriendViewController.swift
@@ -13,12 +13,82 @@ class FriendViewController: UITableViewController {
     weak var delegate: ViewController?
     var friend: Friend!
     
+    var timeZones = [TimeZone]()
+    var selectedTimeZone = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let identifiers = TimeZone.knownTimeZoneIdentifiers
+        identifiers.forEach {
+            if let timeZone = TimeZone(identifier: $0) {
+                timeZones.append(timeZone)
+            }
+        }
+        
+        let now = Date()
+        
+        timeZones.sort {
+            let ourDifference = $0.secondsFromGMT(for: now)
+            let otherDifference = $1.secondsFromGMT(for: now)
+            
+            if ourDifference == otherDifference {
+                return $0.identifier < $1.identifier
+            } else {
+                return ourDifference < otherDifference
+            }
+        }
+        
+        selectedTimeZone = timeZones.index(of: friend.timeZone) ?? 0
     }
 
     @IBAction func nameChanged(_ sender: UITextField) {
     }
     
+    // MARK: - TableView
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        } else {
+            return timeZones.count
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "Name your friend"
+        } else {
+            return "Select their timezone"
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "Name", for: indexPath) as? TextTableViewCell else {
+                fatalError("Couldn't get a text table view cell.")
+            }
+            cell.textField.text = friend.name
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TimeZone", for: indexPath)
+            let timeZone = timeZones[indexPath.row]
+            
+            cell.textLabel?.text = timeZone.identifier
+            
+            let timeDifference = timeZone.secondsFromGMT(for: Date())
+            cell.detailTextLabel?.text = String(timeDifference)
+            
+            if indexPath.row == selectedTimeZone {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+            
+            return cell
+        }
+    }
 }
