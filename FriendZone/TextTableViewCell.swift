a31462a63ab352e3def9b5664d698719cf813bf6
Benjamin Hall <b.e.s.hall@outlook.com>
2019-01-13 18:43:42 +0000

    Added cells to FriendViewController tableview

---
+++ FriendZone/TextTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TextTableViewCell.swift
+//  FriendZone
+//
+//  Created by beshbashbosh on 13/01/2019.
+//  Copyright © 2019 beshbashbosh. All rights reserved.
+//
+
+import UIKit
+
+class TextTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
