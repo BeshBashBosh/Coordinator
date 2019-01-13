446690c44e74a540784bab4771e13d89c6155a12
Benjamin Hall <b.e.s.hall@outlook.com>
2019-01-13 19:16:36 +0000

    Extended int to format time intervals nicely

---
+++ FriendZone/int-TimeFormatting.swift
@@ -0,0 +1,20 @@
+//
+//  int-TimeFormatting.swift
+//  FriendZone
+//
+//  Created by beshbashbosh on 13/01/2019.
+//  Copyright © 2019 beshbashbosh. All rights reserved.
+//
+
+import Foundation
+
+extension Int {
+    func timeString() -> String {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute]
+        formatter.unitsStyle = .positional
+        
+        let formattedString = formatter.string(from: TimeInterval(self)) ?? "0"
+        return formattedString
+    }
+}
